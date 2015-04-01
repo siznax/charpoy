d5f87ae9ce04601046feb90c8a1ec131e598089e
siznax <steve@siznax.net>
2015-03-31 19:42:40 -0700

    enabled Georgian script.

--- static/data.js
+++ static/data.js
@@ -102,7 +102,7 @@ Chars.prototype.ethiopic_a = " ꬁ ꬂ ꬃ ꬄ ꬅ ꬆ ꬉ ꬊ ꬋ ꬌ ꬍ ꬎ �
 // http://en.wikipedia.org/wiki/Greek_and_Coptic
 Chars.prototype.greek = "Ͳ Ͷ Έ Ή Ί Ό Ύ Ώ ΐ Α Β Γ Δ Ε Ζ Η Θ Ι Κ Λ Μ Ν Ξ Ο Π Ρ Σ Τ Υ Φ Χ Ψ Ω Ϊ Ϋ ά έ ή ί ΰ α β γ δ ε ζ η θ ι κ λ μ ν ξ ο π ρ ς σ τ υ φ χ ψ ω ϊ ϋ ό ύ ώ Ϗ ϐ ϑ ϒ ϓ ϔ ϕ ϖ ϗ Ϙ ϙ Ϛ ϛ Ϝ ϝ Ϟ ϟ Ϡ ϡ Ϣ ϣ Ϥ ϥ Ϧ ϧ Ϩ ϩ Ϫ ϫ Ϭ ϭ Ϯ ϯ ϰ ϱ ϲ ϳ ϴ ϵ ϶ Ϸ ϸ Ϲ Ϻ ϻ ϼ Ͻ Ͼ Ͽ".split(' ');
 
-// [unichr(x) for x in range(0x10a0,0x10ff)]
+// http://en.wikipedia.org/wiki/Georgian_scripts
 Chars.prototype.georgian = "Ⴀ Ⴁ Ⴂ Ⴃ Ⴄ Ⴅ Ⴆ Ⴇ Ⴈ Ⴉ Ⴊ Ⴋ Ⴌ Ⴍ Ⴎ Ⴏ Ⴐ Ⴑ Ⴒ Ⴓ Ⴔ Ⴕ Ⴖ Ⴗ Ⴘ Ⴙ Ⴚ Ⴛ Ⴜ Ⴝ Ⴞ Ⴟ Ⴠ Ⴡ Ⴢ Ⴣ Ⴤ Ⴥ ა ბ გ დ ე ვ ზ თ ი კ ლ მ ნ ო პ ჟ რ ს ტ უ ფ ქ ღ ყ შ ჩ ც ძ წ ჭ ხ ჯ ჰ ჱ ჲ ჳ ჴ ჵ ჶ ჷ ჸ ჹ ჺ ჻".split(' ');
 
 // [unichr(x) for x in range(0x05be,0x05f4)]
@@ -172,6 +172,7 @@ Chars.prototype.all = function() {
         .concat(this.ethiopic)
         .concat(this.ethiopic_a)
         .concat(this.greek)
+        .concat(this.georgian)
         .concat(this.han300)
         .concat(this.hebrew)
         .concat(this.hiragana)
