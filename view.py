187f7d79e59073aef8891ecef9520ccd9f6e1762
siznax <steve@siznax.net>
2015-04-24 17:13:22 -0700

    update view.py with /about route.

--- view.py
+++ view.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import itertools
+import json
 import logging
+import os
 import sys
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_bower import Bower
 from frag2text import frag2text
 
@@ -16,10 +19,22 @@ Bower(app)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
+# settings.py
+APP_PATH = os.path.dirname(os.path.abspath(__file__))
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    data_file = os.path.join(APP_PATH, 'static/chars.js')
+    with open(data_file) as json_file:
+        data = json.load(json_file)
+        _list = [data[x]['chars'].split(' ') for x in data]
+        chain = itertools.chain.from_iterable(_list)
+        nchars = len(list(chain))
+        return render_template('about.html',
+                               nchars=nchars,
+                               nblocks=len(data),
+                               data=data)
 
 
 @app.route('/info/<path:url>')
@@ -43,6 +58,12 @@ def info(url):
     except Exception:
         link = '<a target=_blank href="%s">%s</a>' % (url, thing)
         return jsonify(result="&#x2602; Sorry, no info for %s" % link)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(port=5006, debug=True)
